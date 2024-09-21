44b6dc4f0cf5cc4b840c1360022b29d81237837d
zayra77 <52687406+zayra77@users.noreply.github.com>
2024-09-21 12:10:40 +0800

    Add files via upload

---
+++ public/result.php
@@ -0,0 +1,123 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>LEMP prediction result</title>
+<link rel="stylesheet" type="text/css" href="../../style.css" media="screen" />
+</head>
+<body>
+<div id="header">
+    <div id="logo">
+        <h1><a href="#">&nbsp;</a></h1>
+        <p>A predictor of protein crotonylation sites<a href="#"></a></p>
+    </div>
+    <!-- end #logo -->
+    <div id="menu">
+        <ul>
+            <li class="first"><a href="../../index.php?page=introduction">Introduction</a></li>
+            <li><a href="../../index.php?page=prediction">Prediction</a></li>
+            <li><a href="../../index.php?page=help">Help</a></li>
+            <li><a href="../../index.php?page=download">Download</a></li>
+            <li><a href="../../index.php?page=contact">Contact Us</a></li>
+        </ul>
+    </div>
+    <!-- end #menu -->
+</div>
+<div id="page1">
+<a href="./nhKCR_Pred.txt">The text format:</a><br />
+============================================================================================
+<table width="700" border="1" cellspacing="0" cellpadding="0" bordercolor="#000000" class="tableBasic1">
+    <tr>
+        <th width="30%">Sample</th>       
+        <th width="60%">Peptide</th>
+        <th width="10%">Score</th>
+        <th>Prediction</th>
+    </tr>
+    
+<tr>
+<td>O15144_32</td><td class="word">LEVNNRIIEETLALKFENAAAGNKPEAVE</td><td>0.0828</td><td>No</td></tr>
+<tr>
+<td>O15144_41</td><td class="word">ETLALKFENAAAGNKPEAVEVTFADFDGV</td><td>0.0029</td><td>No</td></tr>
+<tr>
+<td>O15144_66</td><td class="word">FDGVLYHISNPNGDKTKVMVSISLKFYKE</td><td>0.2499</td><td>No</td></tr>
+<tr>
+<td>O15144_68</td><td class="word">GVLYHISNPNGDKTKVMVSISLKFYKELQ</td><td>0.0432</td><td>No</td></tr>
+<tr>
+<td>O15144_76</td><td class="word">PNGDKTKVMVSISLKFYKELQAHGADELL</td><td>0.0097</td><td>No</td></tr>
+<tr>
+<td>O15144_79</td><td class="word">DKTKVMVSISLKFYKELQAHGADELLKRV</td><td>0.8829</td><td class="highest">Yes</td></tr>
+<tr>
+<td>O15144_91</td><td class="word">FYKELQAHGADELLKRVYGSFLVNPESGY</td><td>0.8952</td><td class="highest">Yes</td></tr>
+<tr>
+<td>O15144_120</td><td class="word">NVSLLYDLENLPASKDSIVHQAGMLKRNC</td><td>0.0258</td><td>No</td></tr>
+<tr>
+<td>O15144_131</td><td class="word">PASKDSIVHQAGMLKRNCFASVFEKYFQF</td><td>0.7069</td><td class="highest">Yes</td></tr>
+<tr>
+<td>O15144_141</td><td class="word">AGMLKRNCFASVFEKYFQFQEEGKEGENR</td><td>0.0120</td><td>No</td></tr>
+<tr>
+<td>O15144_150</td><td class="word">ASVFEKYFQFQEEGKEGENRAVIHYRDDE</td><td>0.7084</td><td class="highest">Yes</td></tr>
+<tr>
+<td>O15144_171</td><td class="word">VIHYRDDETMYVESKKDRVTVVFSTVFKD</td><td>0.8675</td><td class="highest">Yes</td></tr>
+<tr>
+<td>O15144_172</td><td class="word">IHYRDDETMYVESKKDRVTVVFSTVFKDD</td><td>0.0335</td><td>No</td></tr>
+<tr>
+<td>O15144_184</td><td class="word">SKKDRVTVVFSTVFKDDDDVVIGKVFMQE</td><td>0.4847</td><td class="highest">Yes</td></tr>
+<tr>
+<td>O15144_193</td><td class="word">FSTVFKDDDDVVIGKVFMQEFKEGRRASH</td><td>0.4053</td><td class="high">Yes</td></tr>
+<tr>
+<td>O15144_200</td><td class="word">DDDVVIGKVFMQEFKEGRRASHTAPQVLF</td><td>0.7605</td><td class="highest">Yes</td></tr>
+<tr>
+<td>O15144_224</td><td class="word">PQVLFSHREPPLELKDTDAAVGDNIGYIT</td><td>0.0566</td><td>No</td></tr>
+<tr>
+<td>O15144_270</td><td class="word">NLIHTFRDYLHYHIKCSKAYIHTRMRAKT</td><td>0.0911</td><td>No</td></tr>
+<tr>
+<td>O15144_273</td><td class="word">HTFRDYLHYHIKCSKAYIHTRMRAKTSDF</td><td>0.1683</td><td>No</td></tr>
+<tr>
+<td>O15144_283</td><td class="word">IKCSKAYIHTRMRAKTSDFLKVLNRARPD</td><td>0.7032</td><td class="highest">Yes</td></tr>
+<tr>
+<td>O15144_289</td><td class="word">YIHTRMRAKTSDFLKVLNRARPDAEKKEM</td><td>0.6051</td><td class="highest">Yes</td></tr>
+<tr>
+<td>O15144_300</td><td class="word">DFLKVLNRARPDAEKKEMKTITGKTFSSR</td><td>0.4754</td><td class="highest">Yes</td></tr>
+<tr>
+<td>O15144_301</td><td class="word">FLKVLNRARPDAEKKEMKTITGKTFSSR-</td><td>0.0476</td><td>No</td></tr>
+<tr>
+<td>O15144_304</td><td class="word">VLNRARPDAEKKEMKTITGKTFSSR----</td><td>0.3362</td><td class="high">Yes</td></tr>
+<tr>
+<td>O15144_309</td><td class="word">RPDAEKKEMKTITGKTFSSR---------</td><td>0.6245</td><td class="highest">Yes</td></tr>
+
+</table>
+<br /><br />
+============================================================================================
+<h3>Legend:</h3>
+<table width="450" border="1" cellspacing="0" cellpadding="0">
+    <tr>
+        <td width="83" height="25" align="center" valign="middle" bgcolor="#3399FF"><strong>Label</strong></td>
+        <td width="148" align="center" valign="middle" bgcolor="#3399FF"><strong>Score Range</strong></td>
+        <td width="107" align="center" valign="middle" bgcolor="#3399FF"><strong>Sensitivity </strong>
+        <td width="100" align="center" valign="middle" bgcolor="#3399FF"><strong>Specificity</strong></td>
+    </tr>
+    <tr>
+        <td align="center" valign="middle" bgcolor="#CC66FF"><strong>Medium</strong></td>
+        <td align="center" valign="middle">[0.265 - 0.447) </td>
+        <td align="center" valign="middle">77.75%</td>
+        <td align="center" valign="middle">80.0%</td>
+    </tr>
+    <tr>
+        <td align="center" valign="middle" bgcolor="#FF0000"><strong>High</strong></td>
+        <td align="center" valign="middle">[0.447 - 1] </td>
+        <td align="center" valign="middle">56.63%</td>
+        <td align="center" valign="middle">90.0%</td>
+    </tr>
+</table><br />
+
+</div>
+<!-- end #page -->
+<div id="footer">
+    <p>&copy; 2018. All Rights Reserved. Design by Yzchen.<br />
+    </p>
+</div>
+<!-- end #footer -->
+</body>
+</html>
+    
